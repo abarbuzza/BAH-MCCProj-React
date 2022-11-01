adf3b29427c0b957721b14f46d60b3625e0ae5cc
abarbuzza <55552377+abarbuzza@users.noreply.github.com>
2022-11-01 14:35:11 -0400

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
 
-    app.use(proxy('/api', { target: 'http://10.110.226.66:8080/' }));
-    app.use(proxy('/account', { target: 'http://10.106.39.62:8081/' })); 
+    app.use(proxy('/api', { target: 'http://10.98.210.52:8080/' }));
+    app.use(proxy('/account', { target: 'http://10.104.220.102:8081/' })); 
 	
 	/*app.use(proxy('/api', { target: 'http://172.18.0.2:8080/' }));
 	app.use(proxy('/account', { target: 'http://172.18.0.3:8081/' }));
